4288294099a9011303e3bb370ce75692b553aa5f
Tong <1121433828@qq.com>
2019-09-11 16:44:21 +0800

    公共类，带右父节点信息的二叉树

---
+++ SwordToOffer/src/common/TreeLinkNode.java
@@ -0,0 +1,18 @@
+package common;
+
+/**
+ * @author ：Tong
+ * @date ：Created in 2019/9/11 11:15
+ * @description：
+ * @version: $
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left = null;
+    public TreeLinkNode right = null;
+    public TreeLinkNode next = null;
+
+    public TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
