8cf8acef167110b20dbe4695b8315210b50746c0
Tong <1121433828@qq.com>
2020-03-30 18:00:04 +0800

    leetcode3题

--- LeetCode/src/Top1To10/lengthOfLongestSubstring.java
+++ LeetCode/src/Top1To10/lengthOfLongestSubstring.java
@@ -1,6 +1,7 @@
 package Top1To10;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -11,20 +12,20 @@ import java.util.Set;
  */
 public class lengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
-
         int len = s.length();
-        Set<Character> set = new HashSet<>();
-
+        Set<Character> set = new LinkedHashSet<>();
         int maxLen = 0, leftIndex = 0, rightIndex = 0;
 
         while (rightIndex < len) {
-            if (set.add(s.charAt(rightIndex++))) {
-                int length = rightIndex - leftIndex + 1;
+            if (set.add(s.charAt(rightIndex))) {
+                rightIndex++;
+                int length = rightIndex - leftIndex;
                 if (maxLen < length) {
                     maxLen = length;
                 }
+                rightIndex++;
             } else {
-                set.remove(leftIndex);
+                set.remove(s.charAt(leftIndex));
                 leftIndex++;
             }
         }
