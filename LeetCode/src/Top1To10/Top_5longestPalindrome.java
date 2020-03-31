3642016c3eeb3364edbe72e151d4e295b0f391cb
Tong <1121433828@qq.com>
2020-03-31 17:59:17 +0800

    leetcode3题

---
+++ LeetCode/src/Top1To10/Top_5longestPalindrome.java
@@ -0,0 +1,26 @@
+package Top1To10;
+
+/**
+ * @author ：Tong
+ * @date ：Created in 2020/3/31 10:46
+ * @description：
+ * @version: $
+ */
+public class Top_5longestPalindrome {
+    public String solution(String s) {
+        int len = s.length();
+        if (len <= 1) {
+            return s;
+        }
+        while(len-->1){
+            for (int i = 0; i < ; i++) {
+                
+            }
+        }
+
+    }
+
+    public boolean isHuiString(String s) {
+        return new StringBuffer(s).reverse().toString().equals(s);
+    }
+}
---
+++ LeetCode/src/Top1To10/Top_6convert.java
@@ -0,0 +1,37 @@
+package Top1To10;
+
+/**
+ * @author ：Tong
+ * @date ：Created in 2020/3/31 14:53
+ * @description：
+ * @version: $
+ */
+public class Top_6convert {
+    public String solution(String s, int numRows) {
+        int len = s.length();
+        char[][] strArr = new char[numRows][len / 2];
+
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < len; i++) {
+            if (col % 2 == 0) {//偶数位Z型
+                if (row == 0) {
+                    row = numRows - 1;
+                }
+                if (row == 1) {
+                    col++;
+                    row=0;
+                }
+                strArr[row][col] = s.charAt(i);
+                row--;
+            } else {
+                strArr[row][col] = s.charAt(i);
+                row++;
+                if (row == numRows) {
+                    col++;
+                    row = 0;
+                }
+            }
+        }
+    }
+}
